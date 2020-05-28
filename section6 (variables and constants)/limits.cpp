7bfecc4ab510daa60aea424b522958b1ab9ef59c
MXC1 <mfhcor@gmail.com>
2020-05-28 11:56:49 +0100

    Limits

---
+++ section6 (variables and constants)/limits.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <climits>
+
+using std::cin;
+using std::cout;
+
+int main() {
+    cout << sizeof(long long);
+    // cout << LONG_MAX;
+    cout << LLONG_MAX;
+    cout << WINT_MAX;
+
+    return 0;
+}
