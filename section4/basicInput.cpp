1278bacd075ba8fb3d0a731d2d7087595e07174f
MXC1 <mfhcor@gmail.com>
2020-05-28 10:48:21 +0100

    basicInput

---
+++ section4/basicInput.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main() {
+    cout << "Enter your favourite number: ";
+    
+    double input;
+    
+    cin >> input;
+
+    cout << "No way! " << input << " is my favourite number too!";
+
+    return 0;
+}
